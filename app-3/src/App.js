308d9d63864545d6949813ad06615300f0d6e798
Wyatt King <wyattking@Wyatts-MacBook-Pro.local>
2019-07-09 14:59:56 -0600

    app3, annihilated

--- app-3/src/App.js
+++ app-3/src/App.js
@@ -1,20 +1,34 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      typedList: '',
+      scotch: ['Clynelish', 'Ardbeg', 'Lagavulin', 'Laphroaig', 'Glenfarclas', 'Glenkinchie', 'Bowmore', 'Yamazaki', 'Talisker', 'Balvenie']
+    }
+  }
+  handleTyping(e) {
+    this.setState({
+      typedList: e
+    })
+  }
   render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+    let scotchesToDrink = this.state.scotch
+      .filter(el => {
+        return el.toLowerCase().includes(this.state.typedList.toLowerCase())
+      })
+      .map((el, i) => {
+        return <h2>{++i} {el}</h2>
+      })
+      console.log(this.state.scotch.filter(el => {
+        return el.includes(this.state.typedList)
+      }))
+    return <div className='App'>
+      <input onChange={e => this.handleTyping(e.target.value)} type='text' />
+      {scotchesToDrink}
+    </div>
   }
 }
 
